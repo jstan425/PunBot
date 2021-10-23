2798a1e6eed5fcf69f1349d0fa1c3fc6d9bcb1db
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-10-23 20:29:12 +0800

    Core creation

---
+++ cogs/core/cog.py
@@ -0,0 +1,23 @@
+import disnake
+import logging
+
+from disnake.ext import commands
+
+class Core(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+        
+    @commands.slash_command(description="Prints latency of the bot between server and the host.")
+    async def latency(self,
+                      inter: disnake.ApplicationCommandInteraction):
+        embed = disnake.Embed(
+            title="Latency",
+            description=(f"The latency is {round(self.bot.latency * 1000)}ms"),
+            color=disnake.Color.blue(),
+            )
+        await inter.response.send_message(embed=embed)
+        
+def setup(bot:commands.Bot):
+    bot.add_cog(Core(bot))
+    print("Core cogs is now loaded.\n")
+    
