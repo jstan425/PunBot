9e43de52d7eb5a959e24d9522f7db98974329d9d
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-11-02 18:11:11 +0800

    Update 8ball py

--- cogs/fun/eightball.py
+++ cogs/fun/eightball.py
@@ -14,13 +14,24 @@ class WisdomBall(commands.Cog):
     @commands.slash_command(description="Get 8ball some wisdom.")
     async def eightball(self,
                         inter,
-                        question=Param(None, desc="Ask me a wisdom question, to get a wisdom answer.")):
+                        question=Param(desc="Question you wish to know from 8ball.")):
+             
         possible_response=[
             'Definitely',
             'There is possible',
             'Too hard to tell',
             'It is not looking good',
-            'That is solid NO'
+            'That is solid NO',
+            'You gotta to be kidding, of course is YES!',
+            '...',
+            'No where close to YES',
+            'Sorry what is the question again?',
         ]
-        await inter.response.send_message(random.choice(possible_response) + ", " + disnake.Message.mentions)
+        answer=random.choice(possible_response)
+        embed=disnake.Embed(
+            title="The future says...",
+            description=f"{answer}",
+            color=0x3fff0a
+        )
+        await inter.response.send_message(embed=embed)
     
