7749ba9697ba2f231dec273f5d16ccf4d4073367
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-11-03 20:22:49 +0800

    Git cog creation

--- cogs/core/cog.py
+++ cogs/core/cog.py
@@ -2,13 +2,11 @@ import disnake
 import os
 import logging
 
-
-from enum import Enum
 from disnake.ext import commands
 from disnake.ext.commands import Param
 from utils import check
-from github import Github
-from dotenv import load_dotenv
+from .git import Git
+
 from rich.traceback import install
 install(show_locals=True)
 
@@ -17,19 +15,7 @@ logger = logging.getLogger("punbot")
 class Core(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
-    
-    class Repo(str, Enum):
-        PunBot = "PunBot"
-        
-    class IssueType(str, Enum):
-        Bug = "bug"
-        Documentation = "documentation"
-        Enchancement = "enhancement"
-        NeedHelp = "help wanted"
-        Question = "question"
-        CantFix = "wontfix"
-
-            
+           
     @commands.slash_command(description="Prints latency of the bot between server and the host.")
     async def latency(self,
                       inter: disnake.ApplicationCommandInteraction):
@@ -56,49 +42,21 @@ class Core(commands.Cog):
         print("Cogs is now reloaded.")
         logger.info("Cogs Reloaded")
     
-    @commands.slash_command(description="Create issues in Repo", category = "Administrator")
-    @check.is_admin()
-    async def ghcreate(self,
-                        inter: disnake.ApplicationCommandInteraction,
-                        repo: Repo = Param(desc="Select a repo"),
-                        issuetype: IssueType = Param(desc="Bugs or Enhancements"),
-                        title: str= Param(desc="Title"),
-                        description: str= Param(desc="Description"),
-    ):
-        if inter.guild.id not in [872470314171392001]:
-            embed = disnake.Embed(
-                title="Error",
-                description="Not permitted to create issue",
-                color=disnake.Color.red()
-                )
-            await inter.response.send_message(embed=embed)
-            return
-        
-        load_dotenv()
-        g = Github(os.getenv("GITTOKEN"))
-        if repo == "PunBot": 
-            repo = g.get_repo("jstan425/PunBot")
-        label = repo.get_label(issuetype)
-        g = repo.create_issue(
-            title=title,
-            labels=[label],
-            body=description + "\n\nIssue raised by: " + inter.author.name,
-        )
-        embed= disnake.Embed(
-            title="Thank You!",
-            description="The issue had been raised successfully. You can view it at " + g.html_url,
-            color=0x3fff0a
-            )
-        
-        await inter.response.send_message(embed=embed)
         
 def setup(bot:commands.Bot):
     bot.add_cog(Core(bot))
     print("Core cog is now loaded.\n")
     logger.info("Core cog loaded")
+    bot.add_cog(Git(bot))
+    print("Git cog is now loaded.\n")
+    logger.info("Git cog loaded")
 
 def teardown(bot:commands.Bot):
     bot.remove_cog(Core(bot))
     print("Core cogs is now unloaded.\n")
     logger.info("Core cog unloaded")
+    bot.remove_cog(Git(bot))
+    print("Git cogs is now unloaded.\n")
+    logger.info("Git cog unloaded")
+    
        
---
+++ cogs/core/git.py
@@ -0,0 +1,63 @@
+import disnake
+import logging
+import os
+
+from enum import Enum
+from disnake.ext import commands
+from disnake.ext.commands import Param
+from utils import check
+from github import Github
+from dotenv import load_dotenv
+
+from rich.traceback import install
+
+install(show_locals=True)
+logger = logging.getLogger("punbot")
+logger.setLevel(logging.DEBUG)
+
+class Git(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+        
+    class IssueType(str, Enum):
+        Bug = "bug"
+        Documentation = "documentation"
+        Enchancement = "enhancement"
+        NeedHelp = "help wanted"
+        Question = "question"
+        CantFix = "wontfix"
+        
+    
+    @commands.slash_command(description="Create issues in Repo")
+    @check.is_admin()
+    async def git(self,
+                  inter,
+                  title: str= Param(desc="Title"),
+                  issue_label: IssueType = Param(desc="Bugs or Enhancements"),
+                  description: str= Param(None, desc="Description"),
+    ):
+        if inter.guild.id not in [872470314171392001, 405738567902429244]:
+            embed = disnake.Embed(
+                title="Error",
+                description="Not permitted to create issue",
+                color=disnake.Color.red()
+                )
+            await inter.response.send_message(embed=embed)
+            return
+        
+        load_dotenv()
+        g = Github(os.getenv("GITTOKEN"))
+        repo = g.get_repo("jstan425/PunBot")
+        label = repo.get_label(issue_label)
+        g = repo.create_issue(
+            title=title,
+            labels=[label],
+            body=f"{description}" + "\n\n - " + inter.author.name,
+        )
+        embed= disnake.Embed(
+            title="Thank You!",
+            description="The issue had been raised successfully. You can view it at " + g.html_url,
+            color=0x3fff0a
+            )
+        await inter.response.send_message(embed=embed)
+        
