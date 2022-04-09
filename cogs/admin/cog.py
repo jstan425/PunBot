b2567f901fc0cb1fe833cebee6dd20eaa8dce37d
Joe Tan <tan.jorshun@gmail.com>
2022-04-09 12:22:05 +0000

    Fixed Timeout

--- cogs/admin/cog.py
+++ cogs/admin/cog.py
@@ -1,10 +1,14 @@
 import logging
 import disnake
+import datetime
+import humanfriendly
 
 from disnake.ext import commands
 from disnake.ext.commands import Param
+
 from utils import check
 from utils.formatter import gen_embed
+
 
 logger = logging.getLogger("punbot/ Admin")
 
@@ -33,18 +37,25 @@ class Admin(commands.Cog):
     @check.is_mod()
     async def timeout(self,
             inter,
-            user: disnake.User=Param(desc="Specify a user."),
+            user: disnake.User=Param(desc="Specify a user"),
             duration = Param(desc="Duration to be timeout"),
             reason = Param(None, desc="Reason to be timeout")
         ):
+        # Timeout a member for specific time. 
+        # Use 5m for 5 mins, 1hr for 1 hour etc.
+        if reason is None:
+            reason = 'No reason provided'
+        duration = humanfriendly.parse_timespan(duration)
         embed = gen_embed(
             title="Uh-oh..!",
-            desc=f'{user.name} had been timeout for {reason} for {duration}',
+            desc=f'{user.name} had been timeout for {duration}. \nReason: {reason}',
             msg_type='warning',
         )
-
-        await disnake.Guild.timeout(reason=reason)
-        await inter.response.send_message(embed=embed)
+        try:
+            await disnake.Guild.timeout(self, user=user, until=disnake.utils.utcnow() + datetime.timedelta(seconds=duration), reason=reason)
+            await inter.response.send_message(embed=embed)
+        except disnake.HTTPException as e:
+            await inter.response.send_message("Timeout failed, please contact admin.")
             
 def setup(bot:commands.Bot):
     bot.add_cog(Admin(bot))
